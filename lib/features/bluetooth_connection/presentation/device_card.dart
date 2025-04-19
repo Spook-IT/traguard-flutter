e47bd069bfc04c7a931e9450865689a34668cb5b
Dario Varriale <dario.varriale@atobit.it>
2025-04-19 15:53:13 +0200

    feat: implement DeviceCard widget for displaying Bluetooth device information and connection functionality

---
+++ lib/features/bluetooth_connection/presentation/device_card.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:traguard/features/bluetooth_connection/data/bluetooth_reader.dart';
+import 'package:traguard/utils/assets.dart';
+import 'package:traguard/utils/constants.dart';
+import 'package:traguard/utils/extensions.dart';
+import 'package:traguard/utils/sizes.dart';
+
+/// This widget represents a card that displays
+/// information about a Bluetooth device.
+class DeviceCard extends StatefulWidget {
+  /// Creates a new instance of [DeviceCard].
+  const DeviceCard({required this.device, super.key});
+
+  /// The [ScanResult] object representing the Bluetooth device.
+  final ScanResult device;
+
+  @override
+  State<DeviceCard> createState() => _DeviceCardState();
+}
+
+class _DeviceCardState extends State<DeviceCard> {
+  late final BluetoothDevice _bluetoothDevice = widget.device.device;
+  BluetoothReader? _bluetoothReader;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // TODO(dariowskii): Implement request battery and gps data call
+    });
+  }
+
+  Future<void> _connectToDevice() async {
+    final subscription = _bluetoothDevice.connectionState.listen((
+      BluetoothConnectionState state,
+    ) async {
+      if (state == BluetoothConnectionState.disconnected) {
+        logger.t('${_bluetoothDevice.disconnectReason?.description}');
+      } else if (state == BluetoothConnectionState.connected) {
+        logger.t('Connected to ${_bluetoothDevice.advName}');
+        await _discoverServices(_bluetoothDevice);
+      }
+    });
+    _bluetoothDevice.cancelWhenDisconnected(
+      subscription,
+      delayed: true,
+      next: true,
+    );
+
+    await _bluetoothDevice.connect();
+  }
+
+  Future<void> _discoverServices(BluetoothDevice device) async {
+    final services = await device.discoverServices();
+    final discoveredService = services.firstOrNull;
+
+    if (discoveredService == null) {
+      logger.e('No services found');
+      return;
+    }
+
+    _bluetoothReader = BluetoothReader(
+      connectedDevice: device,
+      service: discoveredService,
+      notifyCaracteristic: discoveredService.characteristics.firstWhere(
+        (element) => element.properties.notify == true,
+        orElse: () => throw Exception('No notify characteristic found'),
+      ),
+      writeCaracteristic: discoveredService.characteristics.firstWhere(
+        (element) => element.properties.write == true,
+        orElse: () => throw Exception('No write characteristic found'),
+      ),
+    );
+
+    unawaited(_bluetoothReader?.listenNotifications());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(color: Colors.grey, blurRadius: 4, offset: Offset(0, 2)),
+        ],
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: Paddings.smallAll,
+            child: Image.asset(ImageAssets.trackerDevice.path, height: 100),
+          ),
+          Expanded(
+            child: Padding(
+              padding: Paddings.smallAll,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.device.advertisementData.advName,
+                        style: context.textTheme.labelLarge,
+                      ),
+                      Text(
+                        _bluetoothDevice.remoteId.str,
+                        style: context.textTheme.labelSmall,
+                      ),
+                    ],
+                  ),
+                  Spaces.medium.sizedBoxHeight,
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: FilledButton(
+                      onPressed: _connectToDevice,
+                      child: const Text('Connect'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
