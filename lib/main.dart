c408708c6726ea0cf4ca134554973fe4aa85482f
Dario Varriale <dario.varriale@atobit.it>
2025-04-19 18:49:04 +0200

    feat: wrap MaterialApp in _EagerInitialization for connected devices state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:traguard/l10n/gen_l10n/app_localizations.dart';
+import 'package:traguard/providers/connected_devices.dart';
 import 'package:traguard/router/router.dart';
 
 void main() {
@@ -14,14 +15,28 @@ class TraguarApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      title: 'Traguard',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return _EagerInitialization(
+      child: MaterialApp.router(
+        title: 'Traguard',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        routerConfig: ref.watch(routerProvider),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: ref.watch(routerProvider),
     );
+  }
+}
+
+class _EagerInitialization extends ConsumerWidget {
+  const _EagerInitialization({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(connectedDevicesProvider);
+    return child;
   }
 }
