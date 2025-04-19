55ba05a95ec4480b05ea577e8c0348ce75a248cd
Dario Varriale <dario.varriale@atobit.it>
2025-04-19 14:50:53 +0200

    feat: add SearchingAnimation widget for displaying a searching animation

---
+++ lib/features/bluetooth_connection/presentation/searching_animation.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:traguard/utils/assets.dart';
+import 'package:traguard/utils/extensions.dart';
+
+/// A widget that displays a searching animation.
+class SearchingAnimation extends StatelessWidget {
+  /// Creates a new instance of [SearchingAnimation].
+  const SearchingAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: context.shortestSide,
+        child: Center(
+          child: Lottie.asset(
+            LottieAssets.bluetoothSearch.path,
+            alignment: Alignment.center,
+            repeat: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
