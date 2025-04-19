414a6af2c3f5fff54bed3c0f957257694608d3f3
Dario Varriale <dario.varriale@atobit.it>
2025-04-19 18:46:05 +0200

    feat: implement ConnectedDevices provider to manage connected devices state

---
+++ lib/providers/connected_devices.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:traguard/models/connected_devices_state.dart';
+
+part 'connected_devices.g.dart';
+
+/// A Riverpod provider that manages the state of connected devices.
+@Riverpod(keepAlive: true)
+class ConnectedDevices extends _$ConnectedDevices {
+  @override
+  ConnectedDevicesState build() {
+    return ConnectedDevicesState(devices: FlutterBluePlus.connectedDevices);
+  }
+
+  /// Adds a device to the list of connected devices.
+  void addDevice(BluetoothDevice device) {
+    if (state.devices.any((d) => d.remoteId == device.remoteId)) {
+      return;
+    }
+    state = state.copyWith(devices: [...state.devices, device]);
+  }
+
+  /// Removes a device from the list of connected devices.
+  void removeDevice(BluetoothDevice device) {
+    state = state.copyWith(
+      devices:
+          state.devices.where((d) => d.remoteId != device.remoteId).toList(),
+    );
+  }
+}
