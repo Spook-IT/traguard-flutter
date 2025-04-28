6e7d9f9aadee960825b5bbb91d0c3d19ab8e31d0
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-28 23:03:06 +0200

    feat(#9): enhance TeamStatisticsPage layout and add StatisticCard components

--- lib/features/team_statistics/presentation/team_statistics.dart
+++ lib/features/team_statistics/presentation/team_statistics.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:traguard/features/team_statistics/presentation/statistic_card.dart';
+import 'package:traguard/utils/extensions.dart';
+import 'package:traguard/utils/sizes.dart';
 
 /// This widget is part of the team statistics feature of the application.
 class TeamStatisticsPage extends StatelessWidget {
@@ -7,9 +10,43 @@ class TeamStatisticsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO(dariowskii): add localization
     return Scaffold(
-      appBar: AppBar(title: const Text('Team Statistics')),
-      body: const Center(child: Text('Team Statistics Screen')),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        padding: Paddings.mediumAll,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Gestione Squadra',
+              style: context.textTheme.headlineMedium?.copyWith(
+                color: context.textTheme.titleLarge?.color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text('Statistiche e analisi di squadra'),
+            Spaces.large.sizedBoxHeight,
+            const StatisticCard(child: Text('Ultima sessione')),
+            Spaces.large.sizedBoxHeight,
+            StatisticCard(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Disponibilità atleti',
+                    style: context.textTheme.labelLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spaces.medium.sizedBoxHeight,
+                  const Text('Statistiche di squadra'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
