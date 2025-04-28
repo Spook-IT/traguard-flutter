a1ead5f617b83012cc71563483cb2fba396c53ae
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-28 22:16:54 +0200

    feat: add light theme configuration for the application

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
 /// A logger instance for logging messages.
 final logger = Logger(printer: PrettyPrinter());
+
+/* 
+ * ----------------
+ *      THEME
+ * ----------------
+ */
+
+/// The light theme for the application.
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6B5CC9)),
+  brightness: Brightness.light,
+  scaffoldBackgroundColor: const Color(0xFFF9FAFB),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.white,
+    foregroundColor: Color(0xFF020817),
+    elevation: 0,
+  ),
+  typography: Typography(black: Typography.blackCupertino),
+);
