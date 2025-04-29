4b5888b4b4479adcb32461781dca3532d915b2d3
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-29 14:09:15 +0200

    feat(#9): implement StatisticsLoadedBody widget for displaying team statistics

---
+++ lib/features/team_statistics_screen/presentation/statistics_loaded_body.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:traguard/features/team_statistics_screen/domain/team_statistics_model.dart';
+import 'package:traguard/features/team_statistics_screen/presentation/statistic_card.dart';
+import 'package:traguard/shared/utils/extensions.dart';
+import 'package:traguard/shared/utils/sizes.dart';
+
+/// This widget is part of the team statistics feature of the application.
+/// It displays the loaded statistics data in a card format.
+class StatisticsLoadedBody extends StatelessWidget {
+  /// Creates a new instance of [StatisticsLoadedBody].
+  const StatisticsLoadedBody({required this.statistics, super.key});
+
+  /// The statistics data to be displayed.
+  final TeamStatisticsModel statistics;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      spacing: Spaces.large,
+      children: [
+        StatisticCard(
+          title: 'Disponibilità atleti',
+          statisticValue: 6,
+          precision: 1,
+          description: 'Attivi: 4, Infortunati: 1, Riposo: 1',
+          bottomChild: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: Spaces.tiny,
+            children: [
+              LinearProgressIndicator(
+                value: 0.67,
+                minHeight: 8,
+                borderRadius: BorderRadius.circular(8),
+                backgroundColor: context.colorScheme.outline.withValues(
+                  alpha: .3,
+                ),
+                color: context.textTheme.labelLarge?.color,
+              ),
+              Text(
+                '67% disponibili',
+                style: context.textTheme.labelSmall?.copyWith(
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const StatisticCard(
+          title: 'Velocità Media Squadra',
+          statisticValue: 26.4,
+          statisticUnit: 'km/h',
+          description: 'Ultima sessione',
+        ),
+        const StatisticCard(
+          title: 'Distanza Totale',
+          statisticValue: 48.8,
+          statisticUnit: 'km',
+          description: 'Ultima sessione',
+        ),
+        const StatisticCard(
+          title: 'Indice Prestazione',
+          statisticValue: 8,
+          description: 'Media di scquadra per ultima sessione',
+        ),
+      ],
+    );
+  }
+}
