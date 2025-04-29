0a87dce84dd361ece0c9cd9590bbd2bb60c48b44
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-29 14:02:22 +0200

    feat(#9): add LoadingStatistics widget for loading state in team statistics

---
+++ lib/features/team_statistics_screen/presentation/loading_statistics.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:traguard/shared/utils/sizes.dart';
+import 'package:traguard/shared/widgets/base_shimmer.dart';
+
+/// This widget is part of the team statistics feature of the application.
+/// It provides a loading state for the statistics section.
+class LoadingStatistics extends StatelessWidget {
+  /// Creates a new instance of [LoadingStatistics].
+  const LoadingStatistics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BaseShimmer(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: Spaces.large,
+        children: [
+          ShimmerContainer(height: 50, width: double.infinity),
+          ShimmerContainer(height: 200, width: double.infinity),
+          ShimmerContainer(height: 200, width: double.infinity),
+        ],
+      ),
+    );
+  }
+}
