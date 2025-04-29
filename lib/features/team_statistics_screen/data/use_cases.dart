cdd19d863d9a0b1ced29362c5a4579f9af37fe9d
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-29 14:04:56 +0200

    feat(#9): add fetchStatistics provider for asynchronous team statistics retrieval

---
+++ lib/features/team_statistics_screen/data/use_cases.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:traguard/features/team_statistics_screen/data/team_statistics_repository.dart';
+import 'package:traguard/features/team_statistics_screen/domain/team_statistics_model.dart';
+
+part 'use_cases.g.dart';
+
+/// This provider fetches the team statistics data.
+/// It uses the [TeamStatisticsRepository] to retrieve the data.
+/// The data is fetched asynchronously and returns a [TeamStatisticsModel].
+@riverpod
+Future<TeamStatisticsModel> fetchStatistics(Ref ref) {
+  // TODO(dariowskii): use real teamId
+  return ref
+      .read(teamStatisticsRepositoryProvider)
+      .getTeamStatistics(teamId: '1');
+}
