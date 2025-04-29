6b79feb31faecdcdd256f0caf3aa4ea8ac8e5148
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-29 12:05:28 +0200

    feat(#9): enhance StatisticCard to include title, statistic value, and description; update TeamStatisticsScreen to utilize new StatisticCard structure

--- lib/features/team_statistics_screen/presentation/statistic_card.dart
+++ lib/features/team_statistics_screen/presentation/statistic_card.dart
@@ -5,21 +5,77 @@ import 'package:traguard/utils/sizes.dart';
 /// This widget is part of the team statistics feature of the application.
 class StatisticCard extends StatelessWidget {
   /// Creates a new instance of [StatisticCard].
-  const StatisticCard({required this.child, super.key});
+  const StatisticCard({
+    required this.title,
+    required this.statisticValue,
+    required this.description,
+    this.statisticUnit,
+    this.bottomChild,
+    super.key,
+  });
 
-  /// The child widget to be displayed inside the card.
-  final Widget child;
+  /// The title of the statistic card.
+  final String title;
+
+  /// The value of the statistic.
+  final String statisticValue;
+
+  /// The description of the statistic.
+  final String description;
+
+  /// The unit of the statistic value (optional).
+  final String? statisticUnit;
+
+  /// A widget to be displayed at the bottom of the card (optional).
+  final Widget? bottomChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: Paddings.mediumAll,
+      padding: Paddings.largeAll,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: context.colorScheme.outline),
+        border: Border.all(
+          color: context.colorScheme.outline.withValues(alpha: .3),
+        ),
       ),
-      child: child,
+      child: Column(
+        spacing: Spaces.small,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: context.textTheme.labelLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            spacing: Spaces.small,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.ideographic,
+            children: [
+              Text(
+                statisticValue,
+                style: context.textTheme.headlineMedium?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              if (statisticUnit != null) ...[Text(statisticUnit!)],
+            ],
+          ),
+          Text(
+            description,
+            style: context.textTheme.labelMedium?.copyWith(
+              color: context.colorScheme.onSurface.withValues(alpha: .5),
+            ),
+          ),
+          if (bottomChild != null) ...[
+            Spaces.tiny.sizedBoxHeight,
+            bottomChild!,
+          ],
+        ],
+      ),
     );
   }
 }
--- lib/features/team_statistics_screen/presentation/team_statistics_screen.dart
+++ lib/features/team_statistics_screen/presentation/team_statistics_screen.dart
@@ -13,38 +13,72 @@ class TeamStatisticsScreen extends StatelessWidget {
     // TODO(dariowskii): add localization
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        padding: Paddings.mediumAll,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Gestione Squadra',
-              style: context.textTheme.headlineMedium?.copyWith(
-                color: context.textTheme.titleLarge?.color,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Text('Statistiche e analisi di squadra'),
-            Spaces.large.sizedBoxHeight,
-            const StatisticCard(child: Text('Ultima sessione')),
-            Spaces.large.sizedBoxHeight,
-            StatisticCard(
-              child: Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          clipBehavior: Clip.none,
+          padding: Paddings.mediumAll,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: Spaces.large,
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Disponibilità atleti',
-                    style: context.textTheme.labelLarge?.copyWith(
+                    'Gestione Squadra',
+                    style: context.textTheme.headlineMedium?.copyWith(
+                      color: context.textTheme.titleLarge?.color,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Spaces.medium.sizedBoxHeight,
-                  const Text('Statistiche di squadra'),
+                  const Text('Statistiche e analisi di squadra'),
                 ],
               ),
-            ),
-          ],
+              StatisticCard(
+                title: 'Disponibilità atleti',
+                statisticValue: '6',
+                description: 'Attivi: 4, Infortunati: 1, Riposo: 1',
+                bottomChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: Spaces.tiny,
+                  children: [
+                    LinearProgressIndicator(
+                      value: 0.67,
+                      minHeight: 8,
+                      borderRadius: BorderRadius.circular(8),
+                      backgroundColor: context.colorScheme.outline.withValues(
+                        alpha: .3,
+                      ),
+                      color: context.textTheme.labelLarge?.color,
+                    ),
+                    Text(
+                      '67% disponibili',
+                      style: context.textTheme.labelSmall?.copyWith(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const StatisticCard(
+                title: 'Velocità Media Squadra',
+                statisticValue: '26.4',
+                statisticUnit: 'km/h',
+                description: 'Ultima sessione',
+              ),
+              const StatisticCard(
+                title: 'Distanza Totale',
+                statisticValue: '48.8',
+                statisticUnit: 'km',
+                description: 'Ultima sessione',
+              ),
+              const StatisticCard(
+                title: 'Indice Prestazione',
+                statisticValue: '8.0',
+                description: 'Media di scquadra per ultima sessione',
+              ),
+            ],
+          ),
         ),
       ),
     );
