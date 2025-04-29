e46d98b74f318ec23a13d603c469c344f238d2a1
Dario Varriale <dariovarriale@rocketmail.com>
2025-04-29 14:01:32 +0200

    feat: add ShimmerContainer widget for customizable shimmer effect

--- lib/shared/widgets/base_shimmer.dart
+++ lib/shared/widgets/base_shimmer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:traguard/shared/utils/extensions.dart';
 
 /// This widget provides a shimmer effect for its child widget.
 class BaseShimmer extends StatelessWidget {
@@ -15,6 +16,39 @@ class BaseShimmer extends StatelessWidget {
       baseColor: Colors.grey,
       highlightColor: Colors.white,
       child: child,
+    );
+  }
+}
+
+/// This widget represents a shimmer container with
+/// a specified height, width, and border radius.
+class ShimmerContainer extends StatelessWidget {
+  /// Creates a new instance of [ShimmerContainer].
+  const ShimmerContainer({
+    required this.height,
+    required this.width,
+    super.key,
+    this.borderRadius = 8,
+  });
+
+  /// The height of the shimmer container.
+  final double height;
+
+  /// The width of the shimmer container.
+  final double width;
+
+  /// The border radius of the shimmer container.
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: context.colorScheme.outline.withValues(alpha: .3),
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
     );
   }
 }
