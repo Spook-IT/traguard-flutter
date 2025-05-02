35c8dc375fffa09b68d4619c028f7c58c5c0f6f5
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-02 18:46:18 +0200

    feat(#7): add loading state to LoginButton

--- lib/features/login_screen/presentation/login_button.dart
+++ lib/features/login_screen/presentation/login_button.dart
@@ -5,18 +5,31 @@ import 'package:traguard/shared/utils/extensions.dart';
 /// This widget represents a login button.
 class LoginButton extends ConsumerWidget {
   /// Creates a new instance of [LoginButton].
-  const LoginButton({super.key, this.onPressed});
+  const LoginButton({super.key, this.onPressed, this.isLoading = false});
 
   /// The callback function to be executed when the button is pressed.
   final VoidCallback? onPressed;
+
+  /// Indicates whether the button is in a loading state.
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SizedBox(
       width: double.infinity,
       child: FilledButton(
-        onPressed: onPressed,
-        child: Text(context.l10n.loginButton),
+        onPressed: isLoading ? null : onPressed,
+        child:
+            isLoading
+                ? const SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: CircularProgressIndicator.adaptive(
+                    strokeWidth: 2,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  ),
+                )
+                : Text(context.l10n.loginButton),
       ),
     );
   }
