179e5b14a76c017e91ebf7cdc7e23556f41d2f38
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-02 15:46:45 +0200

    feat(#1): replace DropdownButton with CommonMenu for improved session selection UI

--- lib/features/team_statistics_screen/presentation/session_filter.dart
+++ lib/features/team_statistics_screen/presentation/session_filter.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:traguard/features/team_statistics_screen/data/session_filter.dart';
+import 'package:traguard/shared/utils/extensions.dart';
+import 'package:traguard/shared/utils/sizes.dart';
+import 'package:traguard/shared/widgets/base_shimmer.dart';
+import 'package:traguard/shared/widgets/common_menu.dart';
 
 class SessionFilterWidget extends ConsumerWidget {
   const SessionFilterWidget({super.key});
@@ -10,26 +14,50 @@ class SessionFilterWidget extends ConsumerWidget {
     final selectionState = ref.watch(sessionFilterProvider);
 
     return SliverToBoxAdapter(
-      child: switch (selectionState) {
-        SessionFilterData(:final sessions, :final selectedSession) =>
-          DropdownButton(
-            value: selectedSession,
-            items:
-                sessions.sessions.map((session) {
-                  return DropdownMenuItem(
+      child: Padding(
+        padding: Paddings.mediumAll,
+        child: switch (selectionState) {
+          SessionFilterData(:final sessions, :final selectedSession) =>
+            CommonMenu(
+              initialSelection: selectedSession,
+              dropdownMenuEntries: [
+                for (final session in sessions.sessions)
+                  DropdownMenuEntry(
                     value: session,
-                    child: Text(session.name),
-                  );
-                }).toList(),
-            onChanged: (session) {
-              ref
-                  .read(sessionFilterProvider.notifier)
-                  .setSelectedSession(session!);
-            },
+                    label: session.name,
+                    labelWidget: Padding(
+                      padding: Paddings.smallVertical,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(session.name),
+                          Text(
+                            context.l10n.longDate(session.date),
+                            style: context.textTheme.labelSmall?.copyWith(
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+              onSelected: (session) {
+                ref
+                    .read(sessionFilterProvider.notifier)
+                    .setSelectedSession(session!);
+              },
+              label: const Text('Le tue sessioni'),
+            ),
+          SessionFilterEmpty() => const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text('Nessuna sessione disponibile'),
           ),
-        SessionFilterEmpty() => const Text('No sessions available'),
-        SessionFilterLoading() => const CircularProgressIndicator(),
-      },
+          SessionFilterLoading() => const BaseShimmer(
+            child: ShimmerContainer(height: 50, width: double.infinity),
+          ),
+        },
+      ),
     );
   }
 }
