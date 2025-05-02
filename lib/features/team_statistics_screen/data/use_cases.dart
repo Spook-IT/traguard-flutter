af99b493c9fd8fb5f07efaf4df27d8d9a63a8128
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-02 14:33:17 +0200

    feat(#1): update fetchStatistics to use teamId parameter and add fetchTeamSessions provider

--- lib/features/team_statistics_screen/data/use_cases.dart
+++ lib/features/team_statistics_screen/data/use_cases.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:traguard/features/team_statistics_screen/data/team_statistics_repository.dart';
+import 'package:traguard/features/team_statistics_screen/domain/team_session.dart';
 import 'package:traguard/features/team_statistics_screen/domain/team_statistics_model.dart';
 
 part 'use_cases.g.dart';
@@ -11,7 +12,14 @@ part 'use_cases.g.dart';
 @riverpod
 Future<TeamStatisticsModel> fetchStatistics(Ref ref) {
   // TODO(dariowskii): use real teamId
-  return ref
-      .read(teamStatisticsRepositoryProvider)
-      .getTeamStatistics(teamId: '1');
+  return ref.read(teamStatisticsRepositoryProvider).getTeamStatistics();
+}
+
+/// This provider fetches the team sessions data.
+/// It uses the [TeamStatisticsRepository] to retrieve the data.
+/// The data is fetched asynchronously and returns a [TeamSessionListModel].
+@riverpod
+Future<TeamSessionListModel> fetchTeamSessions(Ref ref) {
+  // TODO(dariowskii): use real teamId
+  return ref.read(teamStatisticsRepositoryProvider).getTeamSessions();
 }
