54c06e6b4131d189818966e6c400ddebf5206f23
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-06 00:20:48 +0200

    feat(#6): remove StatisticsHeader widget from team statistics feature

--- lib/features/team_statistics_screen/presentation/team_statistics_screen.dart
+++ lib/features/team_statistics_screen/presentation/team_statistics_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:traguard/features/team_statistics_screen/data/use_cases.dart';
 import 'package:traguard/features/team_statistics_screen/presentation/session_filter.dart';
 import 'package:traguard/features/team_statistics_screen/presentation/statistics_dynamic_body.dart';
-import 'package:traguard/features/team_statistics_screen/presentation/statistics_header.dart';
+import 'package:traguard/shared/utils/extensions.dart';
+import 'package:traguard/shared/widgets/traguard_sliver_app_bar.dart';
 
 /// This widget is part of the team statistics feature of the application.
 class TeamStatisticsScreen extends ConsumerWidget {
@@ -13,12 +14,15 @@ class TeamStatisticsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(fetchTeamSessionsProvider);
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
-          StatisticsHeader(),
-          SessionFilterWidget(),
-          StatisticsDynamicBody(),
+          TraguardSliverAppbar(
+            title: context.l10n.teamStatisticsTitle,
+            subtitle: context.l10n.teamStatisticsSubtitle,
+          ),
+          const SessionFilterWidget(),
+          const StatisticsDynamicBody(),
         ],
       ),
     );
