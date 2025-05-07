e8242fb3776b5c98949ff4f7e439c197f845a71a
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-07 11:11:27 +0200

    feat(#6): implement PlayerListBody widget for displaying a list of players based on a query

---
+++ lib/features/player_list_screen/presentation/player_list_body.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:traguard/features/player_list_screen/data/use_cases.dart';
+import 'package:traguard/shared/utils/extensions.dart';
+
+/// This widget is part of the player list feature of the application.
+/// It displays a list of players based on the provided query.
+class PlayerListBody extends ConsumerWidget {
+  /// Creates a new instance of [PlayerListBody].
+  const PlayerListBody({super.key, this.query});
+
+  /// The query string used to filter the list of players.
+  final String? query;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final playersAsync = ref.watch(fetchPlayersProvider(query: query));
+    return switch (playersAsync) {
+      AsyncData(:final value) => SliverList(
+        delegate: SliverChildBuilderDelegate((context, index) {
+          final player = value.players[index];
+          return ListTile(title: Text(player.name));
+        }, childCount: value.players.length),
+      ),
+      AsyncError(:final error) => SliverToBoxAdapter(
+        child: Center(
+          child: Text(error.toString(), style: context.textTheme.bodyLarge),
+        ),
+      ),
+      _ => const SliverToBoxAdapter(
+        child: Center(child: CircularProgressIndicator()),
+      ),
+    };
+  }
+}
