599f52de0ea0a73e2c59c4edd793091a41eb2c60
Dario Varriale <dario.varriale@atobit.it>
2025-05-07 15:52:50 +0200

    feat(#6): refactor PlayerListScreen to use StatefulWidget and implement search functionality

--- lib/features/player_list_screen/presentation/player_list_screen.dart
+++ lib/features/player_list_screen/presentation/player_list_screen.dart
@@ -1,13 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:traguard/features/player_list_screen/presentation/player_list_body.dart';
+import 'package:traguard/features/player_list_screen/presentation/search_player.dart';
 import 'package:traguard/shared/utils/extensions.dart';
 import 'package:traguard/shared/widgets/traguard_sliver_app_bar.dart';
 
 /// This widget is part of the player list feature of the application.
 /// It displays a list of players and their statistics.
-class PlayerListScreen extends StatelessWidget {
+class PlayerListScreen extends StatefulWidget {
   /// Creates a new instance of [PlayerListScreen].
   const PlayerListScreen({super.key});
+
+  @override
+  State<PlayerListScreen> createState() => _PlayerListScreenState();
+}
+
+class _PlayerListScreenState extends State<PlayerListScreen> {
+  late final _textController = TextEditingController();
+
+  Timer? _debounce;
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _debounce?.cancel();
+    _debounce = null;
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    _debounce?.cancel();
+    _debounce = Timer(500.ms, () {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +49,16 @@ class PlayerListScreen extends StatelessWidget {
             titleFactor: 0.15,
             subtitleFactor: 0.4,
           ),
-          const PlayerListBody(),
+          SliverToBoxAdapter(
+            child: SearchPlayer(
+              controller: _textController,
+              onSendValue: _onSearchChanged,
+            ),
+          ),
+          PlayerListBody(
+            query:
+                _textController.text.isNotEmpty ? _textController.text : null,
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
