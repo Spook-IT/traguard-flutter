077cf58eb99dd505df62e3c6262fc217250395fb
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-11 18:27:49 +0200

    feat(#2): update FiscalDataScreen to use localized titles and display fiscal data in a grouped list

--- lib/features/fiscal_data/fiscal_data_screen.dart
+++ lib/features/fiscal_data/fiscal_data_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traguard/shared/utils/extensions.dart';
 
 /// A widget that represents the screen for displaying fiscal data.
 class FiscalDataScreen extends StatelessWidget {
@@ -7,9 +9,28 @@ class FiscalDataScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     return Scaffold(
-      appBar: AppBar(title: const Text('Fiscal Data')),
-      body: const Center(child: Text('Fiscal Data Screen')),
+      appBar: AppBar(title: Text(l10n.fiscalDataTitle)),
+      body: CupertinoListSection.insetGrouped(
+        children: [
+          CupertinoListTile.notched(
+            title: Text(l10n.legalRepresentitive),
+            leading: Icon(Icons.person, color: context.colorScheme.primary),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () {},
+          ),
+          CupertinoListTile.notched(
+            title: Text(l10n.team),
+            leading: Icon(
+              Icons.sports_soccer,
+              color: context.colorScheme.primary,
+            ),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
