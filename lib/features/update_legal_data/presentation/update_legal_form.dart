3c918f48d275eabe9bca22790c4f816b2146792e
Dario Varriale <dariovarriale@rocketmail.com>
2025-05-11 19:47:21 +0200

    feat(#3): enhance UpdateLegalForm with animated transitions for input fields and save button

--- lib/features/update_legal_data/presentation/update_legal_form.dart
+++ lib/features/update_legal_data/presentation/update_legal_form.dart
@@ -45,6 +45,12 @@ class UpdateLegalForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    final delay = 100.ms;
+    final moveYDuration = 300.ms;
+    const moveYCurve = Curves.decelerate;
+    const moveYBegin = 20.0;
+    const moveYEnd = 0.0;
+
     return SliverFillRemaining(
       hasScrollBody: false,
       child: Form(
@@ -56,87 +62,112 @@ class UpdateLegalForm extends StatelessWidget {
             spacing: Spaces.small,
             children: [
               TextFormField(
-                controller: nameController,
-                keyboardType: TextInputType.name,
-                autofillHints: const [
-                  AutofillHints.name,
-                  AutofillHints.familyName,
-                ],
-                textInputAction: TextInputAction.next,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return l10n.nameAndSurnameValidator;
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: '${l10n.nameAndSurname}*',
-                ),
-                onTapOutside: (event) {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                },
-              ),
-
+                    controller: nameController,
+                    keyboardType: TextInputType.name,
+                    autofillHints: const [
+                      AutofillHints.name,
+                      AutofillHints.familyName,
+                    ],
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return l10n.nameAndSurnameValidator;
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: '${l10n.nameAndSurname}*',
+                    ),
+                    onTapOutside: (event) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                  )
+                  .animate()
+                  .moveY(
+                    duration: moveYDuration,
+                    begin: moveYBegin,
+                    end: moveYEnd,
+                    curve: moveYCurve,
+                  )
+                  .fadeIn(),
               TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [AutofillHints.email],
-                textInputAction: TextInputAction.next,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return context.l10n.emailEmpty;
-                  }
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    autofillHints: const [AutofillHints.email],
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return context.l10n.emailEmpty;
+                      }
 
-                  if (!value.isValidEmail) {
-                    return context.l10n.emailInvalid;
-                  }
+                      if (!value.isValidEmail) {
+                        return context.l10n.emailInvalid;
+                      }
 
-                  return null;
-                },
-                decoration: InputDecoration(labelText: '${l10n.email}*'),
-                onTapOutside: (event) {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                },
-              ),
+                      return null;
+                    },
+                    decoration: InputDecoration(labelText: '${l10n.email}*'),
+                    onTapOutside: (event) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                  )
+                  .animate(delay: delay)
+                  .moveY(
+                    duration: moveYDuration,
+                    begin: moveYBegin,
+                    end: moveYEnd,
+                    curve: moveYCurve,
+                  )
+                  .fadeIn(),
               TextFormField(
-                controller: phoneController,
-                keyboardType: TextInputType.phone,
-                autofillHints: const [AutofillHints.telephoneNumber],
-                textInputAction: TextInputAction.done,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(
-                    RegExp(r'^\+?[0-9]*$'),
-                    replacementString: phoneController.text,
-                  ),
-                ],
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return l10n.phoneEmpty;
-                  }
+                    controller: phoneController,
+                    keyboardType: TextInputType.phone,
+                    autofillHints: const [AutofillHints.telephoneNumber],
+                    textInputAction: TextInputAction.done,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'^\+?[0-9]*$'),
+                        replacementString: phoneController.text,
+                      ),
+                    ],
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return l10n.phoneEmpty;
+                      }
 
-                  return null;
-                },
-                decoration: InputDecoration(labelText: '${l10n.phone}*'),
-                onTapOutside: (event) {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                },
-              ),
+                      return null;
+                    },
+                    decoration: InputDecoration(labelText: '${l10n.phone}*'),
+                    onTapOutside: (event) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                  )
+                  .animate(delay: delay * 2)
+                  .moveY(
+                    duration: moveYDuration,
+                    begin: moveYBegin,
+                    end: moveYEnd,
+                    curve: moveYCurve,
+                  )
+                  .fadeIn(),
               const Spacer(),
               SafeArea(
                 child: SaveButton(
-                  isSaving: isSaving,
-                  canSave: canSave,
-                  onPressed: onSave,
-                ),
+                      isSaving: isSaving,
+                      canSave: canSave,
+                      onPressed: onSave,
+                    )
+                    .animate(delay: delay * 3)
+                    .moveY(
+                      duration: 750.ms,
+                      begin: 100,
+                      end: moveYEnd,
+                      curve: Curves.easeOutCubic,
+                    ),
               ),
             ],
           ),
         ),
-      ).animate().fadeIn().moveY(
-        begin: 20,
-        end: 0,
-        duration: 350.ms,
-        curve: Curves.decelerate,
       ),
     );
   }
